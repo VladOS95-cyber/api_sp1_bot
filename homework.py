9f154f17d4dca36b953120a6e55b6f2dfa93ce3a
Vladislav <vladislav.bronzov@gmail.com>
2021-02-09 20:03:25 +0300

    2.0.0

--- homework.py
+++ homework.py
@@ -14,7 +14,7 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-MAIN_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+MAIN_URL = 'https://praktikum.yandex.ru/'
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -28,29 +28,43 @@ logger.addHandler(handler)
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-    if homework_status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    elif homework_status == 'reviewing':
-        return 'работа взята в ревью'
-    else:
-        verdict = 'Ревьюеру всё понравилось, \
+    try:
+        logging.debug('Получение названия домашки')
+        homework_name = homework.get('homework_name')
+        logging.debug('Получение статуса домашки')
+        homework_status = homework.get('status')
+        logging.info('Проверка статуса')
+        if homework_status == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        elif homework_status == 'reviewing':
+            return f'работа "{homework_name}" взята в ревью'
+        else:
+            verdict = 'Ревьюеру всё понравилось, \
 можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    except Exception as error:
+        logging.error(error)
 
 
 def get_homework_statuses(current_timestamp):
+    api = 'api/'
+    user_api = 'user_api/'
+    homework_status = 'homework_statuses/'
     params = {'from_date': current_timestamp}
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     try:
         homework_statuses = requests.get(
-            MAIN_URL,
+            '{0}{1}{2}{3}'.format(
+                MAIN_URL,
+                api,
+                user_api,
+                homework_status),
             params=params,
             headers=headers)
         return homework_statuses.json()
     except requests.exceptions.HTTPError as error:
-        print(error)
+        logging.error(error)
+        return dict()
 
 
 def send_message(message, bot_client):
@@ -63,19 +77,20 @@ def main():
     current_timestamp = int(time.time())
     while True:
         try:
-            new_homework = get_homework_statuses(0)
-            if new_homework.get('homeworks'):
+            new_homework = get_homework_statuses(current_timestamp)
+            getting_homeworks = new_homework.get('homeworks')
+            if getting_homeworks:
                 logging.info('Отправка сообщения')
                 send_message(parse_homework_status(
-                    new_homework.get('homeworks')[0]),
+                    getting_homeworks[0]),
                     bot)
             current_timestamp = new_homework.get(
                 'current_date',
                 current_timestamp)
             time.sleep(1200)
-
+            current_timestamp = int(time.time())
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+            logging.error(f'Бот столкнулся с ошибкой: {e}')
             time.sleep(5)
 
 
